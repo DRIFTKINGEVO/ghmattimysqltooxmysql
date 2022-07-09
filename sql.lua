960e7671bcfa27244c10a0e23c6cdf0a254203fe
Carlos Gil <36943294+DRIFTKINGEVO@users.noreply.github.com>
2022-07-08 22:02:39 -0600

    Create sql.lua

---
+++ sql.lua
@@ -0,0 +1,9 @@
+mysql.async.execute = exports.ghmattimysql.execute
+mysql.async.fetchall = exports.ghmattimysql:execute
+mysql.async.insert =  exports.ghmattimysql:execute
+mysql.sync.fetchall = exports.ghmattimysql:executeSync
+mysql.sync.fetchscalar = exports.ghmattimysql:scalarSync
+mysql.async.fetchscalar = exports.ghmattimysql:scalarSync
+mysql.sync.execute = exports.ghmattimysql:execute
+mysql.async.execute = exports.ghmattimysql:execute
+mysql.ready = exports.ghmattimysql:ready(function()
